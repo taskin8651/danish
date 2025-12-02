e853daafda3dc034450a406027f694cc957f7b52
taskin8651 <mdsayebalam10@gmail.com>
2025-12-02 18:03:43 +0530

    ddf

--- resources/views/custom/master.blade.php
+++ resources/views/custom/master.blade.php
@@ -108,7 +108,7 @@ $links = Link::first();
                                               
                                             </li>
                                             <li class="has-children has-children--multilevel-submenu">
-                                                <a href="/service"><span>Service</span></a>
+                                                <a href="/services"><span>Service</span></a>
                                                 <ul class="submenu">
                                                     @foreach($serviceDetails as $serviceDetail)
                                                     <li class="has-children">
@@ -245,7 +245,7 @@ $links = Link::first();
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-md-6 text-center text-md-start">
-                            <span class="copyright-text">&copy; 2023 Mitech. <a href="https://hasthemes.com/">All Rights Reserved.</a></span>
+                            <span class="copyright-text">&copy; 2025 Clickmint Agency. <a href="https://clickmintagency.in/">All Rights Reserved.</a></span>
                         </div>
                         <div class="col-md-6 text-center text-md-end">
     <ul class="list ht-social-networks solid-rounded-icon">
@@ -380,8 +380,8 @@ $links = Link::first();
                         <div class="col-md-6 col-8">
                             <!-- logo -->
                             <div class="logo">
-                                <a href="index.html">
-                                    <img src="assets/images/logo/logo-dark.webp" class="img-fluid" alt="">
+                                <a href="/">
+                                    <img src="{{ $siteSetting->image_1?->getUrl() ?? 'assets/images/logo/light-logo.webp' }}" class="img-fluid" alt="">
                                 </a>
                             </div>
                         </div>
@@ -396,138 +396,37 @@ $links = Link::first();
             </div>
             <div class="mobile-menu-overlay__body">
                 <nav class="offcanvas-navigation">
-                    <ul>
-                        <li class="has-children">
-                            <a href="index.html">Home</a>
-                            <ul class="sub-menu">
-                                <li><a href="index-infotechno.html"><span>Infotechno</span></a></li>
-                                <li><a href="index-processing.html"><span>Processing</span></a></li>
-                                <li><a href="index-appointment.html"><span>Appointment</span></a></li>
-                                <li><a href="index-services.html"><span>Services</span></a></li>
-                                <li><a href="index-resolutions.html"><span>Resolutions</span></a></li>
-                                <li><a href="index-cybersecurity.html"><span>cybersecurity</span></a></li>
-                                <li><a href="index-modern-it-company.html"><span>Modern IT Company</span></a></li>
-                                <li><a href="index-machine-learning.html"><span>Machine Learning</span></a></li>
-                                <li><a href="index-software-innovation.html"><span>Software Innovation</span></a></li>
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#">Company</a>
-                            <ul class="sub-menu">
-                                <li class="has-children">
-                                    <a href="about-us-01.html"><span>About us</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="about-us-01.html"><span>About us 01</span></a></li>
-                                        <li><a href="about-us-02.html"><span>About us 02</span></a></li>
-                                        <li class="has-children">
-                                            <a href="#"><span>Submenu Level Two</span></a>
-                                            <ul class="sub-menu">
-                                                <li><a href="#"><span>Submenu Level Three</span></a></li>
-                                                <li><a href="#"><span>Submenu Level Three</span></a></li>
-                                                <li><a href="#"><span>Submenu Level Three</span></a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="contact-us.html"><span>Contact us</span></a></li>
-                                <li><a href="leadership.html"><span>Leadership</span></a></li>
-                                <li><a href="why-choose-us.html"><span>Why choose us</span></a></li>
-                                <li><a href="our-history.html"><span>Our history</span></a></li>
-                                <li><a href="faqs.html"><span>FAQs</span></a></li>
-                                <li><a href="careers.html"><span>Careers</span></a></li>
-                                <li><a href="pricing-plans.html"><span>Pricing plans</span></a></li>
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#">IT solutions</a>
-                            <ul class="sub-menu">
-                                <li><a href="it-services.html"><span>IT Services</span></a></li>
-                                <li><a href="managed-it-service.html"><span>Managed IT Services</span></a></li>
-                                <li><a href="industries.html"><span>Industries</span></a></li>
-                                <li><a href="business-solution.html"><span>Business solution</span></a></li>
-                                <li><a href="it-services-details.html"><span>IT Services Details</span></a></li>
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#">Elements</a>
-                            <ul class="sub-menu">
-                                <li class="has-children">
-                                    <a href="#">Element Group 01</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="element-accordion.html"><span>Accordions & Toggles</span></a></li>
-                                        <li><a href="element-box-icon.html"><span>Box Icon</span></a></li>
-                                        <li><a href="element-box-image.html"><span>Box Image</span></a></li>
-                                        <li><a href="element-box-large-image.html"><span>Box Large Image</span></a></li>
-                                        <li><a href="element-buttons.html"><span>Buttons</span></a></li>
-                                        <li><a href="element-cta.html"><span>Call to action</span></a></li>
-                                        <li><a href="element-client-logo.html"><span>Client Logo</span></a></li>
-                                        <li><a href="element-countdown.html"><span>Countdown</span></a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-children">
-                                    <a href="#">Element Group 02</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="element-counters.html"><span>Counters</span></a></li>
-                                        <li><a href="element-dividers.html"><span>Dividers</span></a></li>
-                                        <li><a href="element-flexible-image-slider.html"><span>Flexible image slider</span></a></li>
-                                        <li><a href="element-google-map.html"><span>Google Map</span></a></li>
-                                        <li><a href="element-gradation.html"><span>Gradation</span></a></li>
-                                        <li><a href="element-instagram.html"><span>Instagram</span></a></li>
-                                        <li><a href="element-lists.html"><span>Lists</span></a></li>
-                                        <li><a href="element-message-box.html"><span>Message box</span></a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-children">
-                                    <a href="#">Element Group 03</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="element-popup-video.html"><span>Popup Video</span></a></li>
-                                        <li><a href="element-pricing-box.html"><span>Pricing Box</span></a></li>
-                                        <li><a href="element-progress-bar.html"><span>Progress Bar</span></a></li>
-                                        <li><a href="element-progress-circle.html"><span>Progress Circle</span></a></li>
-                                        <li><a href="element-rows-columns.html"><span>Rows & Columns</span></a></li>
-                                        <li><a href="element-social-networks.html"><span>Social Networks</span></a></li>
-                                        <li><a href="element-tabs.html"><span>Tabs</span></a></li>
-                                        <li><a href="element-team-member.html"><span>Team member</span></a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-children">
-                                    <a href="#">Element Group 04</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="element-testimonials.html"><span>Testimonials</span></a></li>
-                                        <li><a href="element-timeline.html"><span>Timeline</span></a></li>
-                                        <li><a href="element-carousel-sliders.html"><span>Carousel Sliders</span></a></li>
-                                        <li><a href="element-typed-text.html"><span>Typed Text</span></a></li>
-                                    </ul>
-                                </li>
-
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#">Case Studies</a>
-                            <ul class="sub-menu">
-                                <li><a href="case-studies.html"><span>Case Studies 01</span></a></li>
-                                <li><a href="case-studies-02.html"><span>Case Studies 02</span></a></li>
-                                <li><a href="single-smart-vision.html"><span>Single Layout</span></a></li>
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#">Blogs</a>
-                            <ul class="sub-menu">
-                                <li><a href="blog-list-large-image.html"><span>List Large Image</span></a></li>
-                                <li><a href="blog-list-left-large-image.html"><span>Left Large Image</span></a></li>
-                                <li><a href="blog-grid-classic.html"><span>Grid Classic</span></a></li>
-                                <li><a href="blog-grid-masonry.html"><span>Grid Masonry</span></a></li>
-                                <li class="has-children">
-                                    <a href="blog-post-layout-one.html"><span>Single Layouts</span></a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog-post-layout-one.html"><span>Left Sidebar</span></a></li>
-                                        <li><a href="blog-post-right-sidebar.html"><span>Right Sidebar</span></a></li>
-                                        <li><a href="blog-post-no-sidebar.html"><span>No Sidebar</span></a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
+                     <ul>
+                                            <li class="">
+                                                <a href="/"><span>Home</span></a>
+                                              
+                                            </li>
+                                            <li class="">
+                                                <a href="#"><span>About</span></a>
+                                              
+                                            </li>
+                                            <li class="has-children has-children--multilevel-submenu">
+                                                <a href="/services"><span>Service</span></a>
+                                                <ul class="submenu">
+                                                    @foreach($serviceDetails as $serviceDetail)
+                                                    <li class="has-children">
+                                                        <a href="{{ route('custom.serviceDetail', $serviceDetail->id) }}">
+                                                            <span>{{ $serviceDetail->title }}</span></a>
+                                                        </li>
+                                                    @endforeach
+                                                   
+                                                </ul>
+                                            </li>
+                                          
+                                         <li class="">
+                                                <a href="/gallery"><span>Gallery</span></a>
+                                              
+                                            </li>
+                                            <li class="">
+                                                <a href="/contact"><span>Contact</span></a>
+                                              
+                                            </li>
+                                        </ul>
                 </nav>
             </div>
         </div>
